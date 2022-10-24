c7ac3c0851612ca3dba2937ac688977a3c724c40
K0N0HA <107436295+K0N0HATV@users.noreply.github.com>
2022-10-25 01:14:24 +0300

    add polifil

--- Pro-14/polifil.js
+++ Pro-14/polifil.js
@@ -1,18 +1,18 @@
-// 'use strict'
+'use strict'
 
-// if (!Array.prototype.max) {
-//     Array.prototype.max = function () {
-//         let obj = Object(this)
-//         let count = 0
+if (!Array.prototype.max) {
+    Array.prototype.max = function () {
+        let obj = Object(this)
+        let count = 0
 
-//         for (let i = 0; i < obj.length; i++) {
-//             if (Object(this)[i] > count) {
-//                 count = Object(this)[i]
-//             }
-//         }
+        for (let i = 0; i < obj.length; i++) {
+            if (Object(this)[i] > count) {
+                count = Object(this)[i]
+            }
+        }
 
-//         return console.log(count)
-//     }
-// }
+        return console.log(count)
+    }
+}
 
-// [6, 5, 8, 7].max()
+[6, 5, 8, 7].max()
