25cac37747eb5a2f3fb200b287f741cbb6c05cdf
K0N0HA <107436295+K0N0HATV@users.noreply.github.com>
2022-10-25 01:09:55 +0300

    add hw-14

---
+++ Pro-14/polifil.js
@@ -0,0 +1,18 @@
+// 'use strict'
+
+// if (!Array.prototype.max) {
+//     Array.prototype.max = function () {
+//         let obj = Object(this)
+//         let count = 0
+
+//         for (let i = 0; i < obj.length; i++) {
+//             if (Object(this)[i] > count) {
+//                 count = Object(this)[i]
+//             }
+//         }
+
+//         return console.log(count)
+//     }
+// }
+
+// [6, 5, 8, 7].max()
