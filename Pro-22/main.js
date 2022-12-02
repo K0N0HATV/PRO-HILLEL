a41882437bf04c442556c2674355eb17350b6cd3
K0N0HA <107436295+K0N0HATV@users.noreply.github.com>
2022-12-02 16:40:53 +0200

    change hm-22

--- Pro-22/main.js
+++ Pro-22/main.js
@@ -1,68 +1,24 @@
 const SELECTOR_ID_NOTES_LIST = '#notesList'
-const SELECTOR_ID_FORM = '#dialog-form form'
 const CLASS_DELETE_ITEM = 'notesItemDelete'
 const SELECTOR_ID_NOTES_ITEM = '#notesItem'
 const ATTRIBUTE_DATA_ID = 'id'
 const CLASS_NOTES_ITEM_TEXT_AREA = 'notesItemText'
 const SELECTOR_ID_DESCRIPTION = '#description'
-const SELECTOR_ID_INPUT = '#id'
-const SELECTOR_CLASS_INPUTS = '.inp'
 const SELECTOR_ID_DIALOG_FORM = "#dialog-form"
-const FIRST_ELEMENT = 0
 const SELECTOR_ID_FORM_ADD_STICER = '#form'
 const OPEN_DIALOG_FORM = "open"
 const CLOSE_DIALOG_FORM = "close"
 
+const SELECTOR_CLASS_INPUTS = '.inp'
+
 const $notesListContainer = $(SELECTOR_ID_NOTES_LIST)
 const $formAddSticer = $(SELECTOR_ID_FORM_ADD_STICER)
 const $descriptionInp = $(SELECTOR_ID_DESCRIPTION)
-const $id = $(SELECTOR_ID_INPUT)
+
 const $inputs = $(SELECTOR_CLASS_INPUTS)
-const $formModal = $(SELECTOR_ID_FORM)
+
 let notesList = []
-const dialog = $(SELECTOR_ID_DIALOG_FORM).dialog({
-    autoOpen: false,
-    height: 400,
-    width: 350,
-    modal: true,
-    buttons: {
-        Save: () => {
-            const note = getNote();
-            const $noteId = $id.val()
-
-            if (textAreaValid(note)) {
-                showError({ message: 'поле не должно быть пустое' })
-                return
-            }
-
-            if ($noteId) {
-                NotesApi.update($noteId, note)
-                    .catch(showError)
-
-                const noteOld = notesList.find(item => item.id === $noteId);
-                Object.keys(note).forEach(key => noteOld[key] = note[key]);
-
-                renderNotesList(notesList);
-            } else {
-                NotesApi.create(note)
-                    .then((newSticer) => {
-                        notesList.push(newSticer);
-                        renderNotesList(notesList);
-                    })
-                    .catch(showError)
-                renderNote(note)
-            }
-
-            clearForm()
-        },
-        Cancel: function () {
-            clearForm()
-        }
-    },
-    close: function () {
-        clearForm()
-    }
-});
+const modal = new FormModal(SELECTOR_ID_DIALOG_FORM)
 
 $formAddSticer.on('submit', onFormElSubmit)
 $notesListContainer.on('click', onNotesListContainerClick)
@@ -72,7 +28,7 @@ getNotesList()
 function onFormElSubmit(e) {
     e.preventDefault()
 
-    dialog.dialog(OPEN_DIALOG_FORM);
+    modal.open()
 }
 
 function onNotesListContainerClick(e) {
@@ -91,8 +47,8 @@ function onNotesListContainerClick(e) {
         const oldNote = notesList.find(el => +el.id === $noteId)
 
         fillForm(oldNote)
-
-        dialog.dialog(OPEN_DIALOG_FORM);
+        
+        modal.open()
     }
 }
 
@@ -105,7 +61,9 @@ function getNotesList() {
 
 function fillForm(note) {
     for (const input of $inputs) {
-        input.value = note[input.id]
+        if (note[input.id]) {
+            input.value = note[input.id]
+        }
     }
 }
 
@@ -134,16 +92,6 @@ function getNote() {
 
 function hasClass(el, classItem) {
     return el.hasClass(classItem)
-}
-
-function textAreaValid(el) {
-    return el.description.trim() === ''
-}
-
-function clearForm() {
-    $formModal[FIRST_ELEMENT].reset();
-    $id[FIRST_ELEMENT].value = ''
-    dialog.dialog(CLOSE_DIALOG_FORM);
 }
 
 function showError(err) {
